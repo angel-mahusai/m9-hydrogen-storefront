fb061e808fd87ea1a76214ed3100a8390b2732ef
Angel Lacson <angel@bld.ai>
2025-09-18 01:20:05 +0800

    feat: enable filtering for collection page

--- app/routes/collections.$handle.tsx
+++ app/routes/collections.$handle.tsx
@@ -11,11 +11,15 @@ import {PaginatedResourceSection} from '~/components/PaginatedResourceSection';
 import {redirectIfHandleIsLocalized} from '~/lib/redirect';
 import {ProductItem} from '~/components/ProductItem';
 import {PRODUCT_ITEM_FRAGMENT} from '~/lib/fragments';
-import {useMemo, useRef, useState} from 'react';
-import {ChevronDownIcon, FilterIcon} from '~/assets';
+import {useEffect, useMemo, useRef, useState} from 'react';
+import {ChevronDownIcon, FilterIcon, MinusIcon, PlusIcon} from '~/assets';
 import {useClickOutside} from '~/lib/utils';
 import {PRODUCT_COLLECTION_SORT_MAPPING} from '~/lib/constants';
 import ImageWithText from '~/components/ImageWithText';
+import type {
+  Collection,
+  ProductFilter,
+} from '@shopify/hydrogen/storefront-api-types';
 
 export const meta: MetaFunction<typeof loader> = ({data}) => {
   return [
@@ -56,21 +60,63 @@ async function loadCriticalData({
     throw redirect('/collections');
   }
 
-  const sortAndFilterVariables: Record<string, any> = {};
+  const sortVariables: Record<string, any> = {};
+  // const filterVariables: Record<string, any> = {};
+  const filters: ProductFilter[] = [];
+  const priceFilter: Record<string, any> = {};
+
   const url = new URL(request.url);
   const sortKey = url.searchParams.get(
     'sort_by',
   ) as keyof typeof PRODUCT_COLLECTION_SORT_MAPPING;
-  if (sortKey && sortKey in PRODUCT_COLLECTION_SORT_MAPPING) {
-    sortAndFilterVariables['sortKey'] =
-      PRODUCT_COLLECTION_SORT_MAPPING[sortKey].value;
-    sortAndFilterVariables['reverse'] =
-      PRODUCT_COLLECTION_SORT_MAPPING[sortKey].reverse;
+
+  Array.from(url.searchParams).map(([key, value]) => {
+    if (key === 'sort_by' && value in PRODUCT_COLLECTION_SORT_MAPPING) {
+      sortVariables['sortKey'] = PRODUCT_COLLECTION_SORT_MAPPING[sortKey].value;
+      sortVariables['reverse'] =
+        PRODUCT_COLLECTION_SORT_MAPPING[sortKey].reverse;
+      return;
+    }
+
+    if (!key.startsWith('filter')) {
+      return;
+    }
+
+    if (key.startsWith('filter.v.price')) {
+      const priceBoundary = key.endsWith('.gte') ? 'min' : 'max';
+      priceFilter[priceBoundary] = Number(value);
+    } else if (key.startsWith('filter.v.availability')) {
+      filters.push({available: value === 'In stock' ? true : false});
+    } else if (key.startsWith('filter.v.option')) {
+      const variantName = key.split('.')[3];
+      filters.push({variantOption: {name: variantName, value}});
+    } else if (key.startsWith('filter.p.m.')) {
+      const metaobjectNamespace = key.split('.')[3];
+      const metaobjectKey = key.split('.')[4];
+      const metaobjectValue = `gid:\/\/shopify\/Metaobject\/${value.split('-').pop()}`;
+      filters.push({
+        productMetafield: {
+          namespace: metaobjectNamespace,
+          key: metaobjectKey,
+          value: metaobjectValue,
+        },
+      });
+    }
+  });
+
+  if (Object.keys(priceFilter).length > 0) {
+    filters.push({price: priceFilter});
   }
 
   const [{collection}] = await Promise.all([
     storefront.query(COLLECTION_QUERY, {
-      variables: {handle, ...paginationVariables, ...sortAndFilterVariables},
+      variables: {
+        handle,
+        ...paginationVariables,
+        ...sortVariables,
+        filters,
+        // filters: filterVariables,
+      },
       // Add other queries here, so that they are loaded in parallel
     }),
   ]);
@@ -86,6 +132,7 @@ async function loadCriticalData({
 
   return {
     collection,
+    filters,
   };
 }
 
@@ -104,8 +151,7 @@ interface SocialLink {
 }
 
 export default function Collection() {
-  const {collection, sortKey} = useLoaderData<typeof loader>();
-
+  const {collection, filters, sortKey} = useLoaderData<typeof loader>();
   const {pathname, search} = useLocation();
   const searchParams = useMemo(() => new URLSearchParams(search), [search]);
   const navigate = useNavigate();
@@ -118,6 +164,33 @@ export default function Collection() {
           sortKey as keyof typeof PRODUCT_COLLECTION_SORT_MAPPING
         ]
       : null;
+
+  const priceFilter = collection.products.filters.find(
+    (filter) => filter.type === 'PRICE_RANGE',
+  );
+  const [minPrice, setMinPrice] = useState<number | undefined>(
+    priceFilter
+      ? searchParams?.get('filter.v.price.gte') ||
+          JSON.parse(priceFilter.values[0].input || '')['price']['min']
+      : undefined,
+  );
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(
+    priceFilter
+      ? searchParams?.get('filter.v.price.lte') ||
+          JSON.parse(priceFilter.values[0].input || '')['price']['max']
+      : undefined,
+  );
+
+  useEffect(() => {
+    setMinPrice(
+      searchParams?.get('filter.v.price.gte') ||
+        JSON.parse(priceFilter.values[0].input || '')['price']['min'],
+    );
+    setMaxPrice(
+      searchParams?.get('filter.v.price.lte') ||
+        JSON.parse(priceFilter.values[0].input || '')['price']['max'],
+    );
+  }, [priceFilter, searchParams]);
 
   const collectionType = collection.collection_type?.value;
   const collectionImage =
@@ -138,6 +211,52 @@ export default function Collection() {
   useClickOutside(sortDropdownRef, () => {
     setIsSortOpen(false);
   });
+
+  function clearFilters() {
+    let filterSearchParams;
+    if (!searchParams) {
+      filterSearchParams = new URLSearchParams(search);
+    } else {
+      filterSearchParams = searchParams;
+    }
+
+    Array.from(filterSearchParams).map(([key, value]) => {
+      if (key.startsWith('filter')) {
+        filterSearchParams.delete(key);
+      }
+    });
+
+    navigate(
+      {
+        pathname,
+        search: filterSearchParams.toString(),
+      },
+      {preventScrollReset: true},
+    );
+  }
+
+  interface FilterClear {
+    filterId: string;
+    filterValue?: string;
+  }
+
+  function clearFilter(filtersToClear: FilterClear[]) {
+    filtersToClear.map((filter) => {
+      if (filter.filterValue) {
+        searchParams.delete(filter.filterId, filter.filterValue);
+      } else {
+        searchParams.delete(filter.filterId);
+      }
+    });
+
+    navigate(
+      {
+        pathname,
+        search: searchParams.toString(),
+      },
+      {preventScrollReset: true},
+    );
+  }
 
   return (
     <div className="collection">
@@ -266,10 +385,13 @@ export default function Collection() {
                       onClick={() => {
                         setIsSortOpen(false);
                         searchParams.set('sort_by', key);
-                        navigate({
-                          pathname,
-                          search: searchParams.toString(),
-                        });
+                        navigate(
+                          {
+                            pathname,
+                            search: searchParams.toString(),
+                          },
+                          {preventScrollReset: true},
+                        );
                       }}
                       style={
                         searchParams.get('sort_by') === key
@@ -290,7 +412,86 @@ export default function Collection() {
         </div>
         <div className="products-grid-wrapper">
           <div className={`product-filters${isFilterOpen ? ' open' : ''}`}>
-            FILTERS
+            <div>
+              {searchParams && (
+                <div className="active-filters">
+                  {searchParams.size >
+                    1 + (searchParams.has('sort_by') ? 1 : 0) && (
+                    <button onClick={clearFilters}>Clear All</button>
+                  )}
+                  {(collection.products.filters || []).map((filter) => {
+                    if (
+                      filter.type === 'PRICE_RANGE' &&
+                      (searchParams.has('filter.v.price.gte') ||
+                        searchParams.has('filter.v.price.lte'))
+                    ) {
+                      return (
+                        <button
+                          key={`active-filter-${filter.id}`}
+                          onClick={() => {
+                            clearFilter([
+                              {filterId: 'filter.v.price.gte'},
+                              {filterId: 'filter.v.price.lte'},
+                            ]);
+                          }}
+                        >
+                          {`₱${searchParams.get('filter.v.price.gte')} - ₱${searchParams.get('filter.v.price.lte')}`}
+                        </button>
+                      );
+                    } else if (filter.type === 'LIST') {
+                      return (
+                        <>
+                          {filter.values.map((filterOption) => {
+                            const searchParamValue = filter.id.startsWith(
+                              'filter.p.m.',
+                            )
+                              ? `${filterOption.label}-${filterOption.id.split('-').pop()}`
+                              : filterOption.label;
+                            return (
+                              searchParams.has(filter.id, searchParamValue) && (
+                                <button
+                                  key={`active-filter-${filterOption.id}`}
+                                  onClick={() => {
+                                    clearFilter([
+                                      {
+                                        filterId: filter.id,
+                                        filterValue: searchParamValue,
+                                      },
+                                    ]);
+                                  }}
+                                >
+                                  {filterOption.label}
+                                </button>
+                              )
+                            );
+                          })}
+                        </>
+                      );
+                    }
+                    return <></>;
+                  })}
+                </div>
+              )}
+              {(collection.products.filters || []).map((filter) => {
+                return (
+                  <FilterListOptions
+                    filter={filter}
+                    searchParams={searchParams}
+                    key={filter.id}
+                    minPrice={minPrice}
+                    maxPrice={maxPrice}
+                    setMinPrice={setMinPrice}
+                    setMaxPrice={setMaxPrice}
+                  />
+                );
+              })}
+            </div>
+            <button
+              className="button reset-filters"
+              onClick={() => clearFilters()}
+            >
+              RESET
+            </button>
           </div>
           <PaginatedResourceSection
             connection={collection.products}
@@ -318,6 +519,156 @@ export default function Collection() {
   );
 }
 
+function FilterListOptions({
+  filter,
+  searchParams,
+  minPrice,
+  maxPrice,
+  setMinPrice,
+  setMaxPrice,
+}: {
+  filter: Collection['products']['filters'][0];
+  searchParams?: URLSearchParams;
+  minPrice: number | undefined;
+  maxPrice: number | undefined;
+  setMinPrice: React.Dispatch<React.SetStateAction<number | undefined>>;
+  setMaxPrice: React.Dispatch<React.SetStateAction<number | undefined>>;
+}) {
+  const navigate = useNavigate();
+  const {pathname, search} = useLocation();
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  if (!filter) return null;
+
+  function handlePriceSet() {
+    let filterSearchParams;
+    if (!searchParams) {
+      filterSearchParams = new URLSearchParams(search);
+    } else {
+      filterSearchParams = searchParams;
+    }
+
+    filterSearchParams.set(`${filter.id}.gte`, (minPrice || '0').toString());
+    filterSearchParams.set(`${filter.id}.lte`, (maxPrice || '').toString());
+    navigate(
+      {
+        pathname,
+        search: filterSearchParams.toString(),
+      },
+      {preventScrollReset: true},
+    );
+  }
+
+  function handleAddFilter(value: string) {
+    let filterSearchParams;
+    if (!searchParams) {
+      filterSearchParams = new URLSearchParams(search);
+    } else {
+      filterSearchParams = searchParams;
+    }
+
+    if (filterSearchParams.has(filter.id, value)) {
+      filterSearchParams.delete(filter.id, value);
+    } else {
+      filterSearchParams.append(filter.id, value);
+    }
+
+    navigate(
+      {
+        pathname,
+        search: filterSearchParams.toString(),
+      },
+      {preventScrollReset: true},
+    );
+  }
+
+  return (
+    <div className={`filter-category${open ? ' open' : ''}`}>
+      <div className="filter-label">
+        <h4>{filter.label}</h4>
+        <button onClick={() => setOpen(!open)}>
+          <PlusIcon
+            width={18}
+            height={18}
+            stroke="rgba(var(--color-secondary-text), 0.5)"
+            viewBox="0 0 24 24"
+          />
+          <MinusIcon
+            width={18}
+            height={18}
+            stroke="rgba(var(--color-secondary-text), 0.5)"
+            viewBox="0 0 24 24"
+          />
+        </button>
+      </div>
+      {filter.type === 'LIST' ? (
+        <ul className="filter-options">
+          {filter.values.map((filterOption) => {
+            const searchParamValue = filter.id.startsWith('filter.p.m.')
+              ? `${filterOption.label}-${filterOption.id.split('-').pop()}`
+              : filterOption.label;
+            return (
+              <li key={filterOption.id}>
+                <input
+                  type="checkbox"
+                  onClick={() => handleAddFilter(searchParamValue)}
+                  checked={
+                    searchParams
+                      ? searchParams.has(filter.id, searchParamValue)
+                      : false
+                  }
+                />
+                <span>{filterOption.label}</span>
+              </li>
+            );
+          })}
+        </ul>
+      ) : filter.type === 'PRICE_RANGE' ? (
+        <div className="price-filter">
+          <div className="price-input-wrapper">
+            <span className="currency-symbol">₱</span>
+            <input
+              className="price-range"
+              id="price-range-min"
+              value={minPrice || 0}
+              onChange={(e) =>
+                setMinPrice(
+                  Number(e.target.value) < 0
+                    ? undefined
+                    : Number(e.target.value),
+                )
+              }
+              onBlur={() => handlePriceSet()}
+              type="text"
+            />
+          </div>
+          <span>-</span>
+          <div className="price-input-wrapper">
+            <span className="currency-symbol">₱</span>
+            <input
+              className="price-range"
+              id="price-range-max"
+              value={maxPrice || ''}
+              onChange={(e) =>
+                setMaxPrice(
+                  Number(e.target.value) < 0
+                    ? undefined
+                    : Number(e.target.value),
+                )
+              }
+              onBlur={() => handlePriceSet()}
+              type="text"
+            />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
 // NOTE: https://shopify.dev/docs/api/storefront/2022-04/objects/collection
 const COLLECTION_QUERY = `#graphql
   ${PRODUCT_ITEM_FRAGMENT}
@@ -331,6 +682,7 @@ const COLLECTION_QUERY = `#graphql
     $endCursor: String
     $sortKey: ProductCollectionSortKeys
     $reverse: Boolean
+    $filters: [ProductFilter!]
   ) @inContext(country: $country, language: $language) {
     collection(handle: $handle) {
       id
@@ -415,6 +767,7 @@ const COLLECTION_QUERY = `#graphql
       products(
         sortKey: $sortKey,
         reverse: $reverse,
+        filters: $filters,
         first: $first,
         last: $last,
         before: $startCursor,
@@ -422,6 +775,18 @@ const COLLECTION_QUERY = `#graphql
       ) {
         nodes {
           ...ProductItem
+        }
+        filters {
+          id
+          label
+          type
+          values {
+            count
+            id
+            input
+            label
+            swatch {color}
+          }
         }
         pageInfo {
           hasPreviousPage
